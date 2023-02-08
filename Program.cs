33e3b5fe76ce6fe25dd783d6dd38ef2d4be124c9
ankit <ankit@ANKIT-CD10D75V>
2023-02-09 00:51:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrandSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Select Module to run");
+                Console.WriteLine("1. Hello World");
+                Console.WriteLine("Press Escape to Exit");
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
